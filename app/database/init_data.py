048113181ba65c5630e91e35aa21561073284c8a
Daniel Nery <danielpontesnery@gmail.com>
2022-11-13 18:47:43 -0300

    feat: Commit inicial.

---
+++ app/database/init_data.py
@@ -0,0 +1,15 @@
+
+
+from app.documents.users.user import UserCreateDocument
+from app.core import security
+
+
+#TODO: tO CRIANDO...
+
+# def init_data(user: UserCreateDocument)
+
+#     default_user = UserCreateDocument(
+#         **user,
+#         "nm_password": security.get_password_hash(user["nm_password"])
+#     )
+
